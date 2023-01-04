25156031c79e46df9e0a7e76d320d1fd785c51a3
Baimenovbn <baibn@mail.ru>
2023-01-04 20:10:32 +0600

    feat: more elegant solution

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,34 +1,25 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
         ArrayList<Integer> orderedDishesToEat = new ArrayList<>();
-        ArrayList<Integer> dishesToEat = new ArrayList<>();
+        ArrayDeque<Integer> dishesToEat = new ArrayDeque<>();
 
         for (int i = 1; i <= numberOfDishes; i++) {
             dishesToEat.add(i);
         }
 
-        int i = 0;
-        int countIndex = 1;
-        while (!dishesToEat.isEmpty() && orderedDishesToEat.size() != numberOfDishes) {
+        int i = 1;
+        while (!dishesToEat.isEmpty()) {
 
-            if (dishesToEat.get(i) != null) {
-                if (countIndex % everyDishNumberToEat == 0) {
-                    orderedDishesToEat.add(dishesToEat.get(i));
-                    dishesToEat.set(i, null);
-                    countIndex = 1;
-                } else {
-                    countIndex++;
-                }
-            }
-
-
-            if (i == dishesToEat.size() - 1) {
-                i = -1;
+            if (i % everyDishNumberToEat == 0) {
+                orderedDishesToEat.add(dishesToEat.pollFirst());
+            } else {
+                dishesToEat.add(dishesToEat.pollFirst());
             }
 
             i++;
