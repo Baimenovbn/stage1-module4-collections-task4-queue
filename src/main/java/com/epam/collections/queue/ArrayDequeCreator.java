9488bc683c4fb032761989d6874abab21acb1257
Baimenovbn <baibn@mail.ru>
2023-01-04 15:26:43 +0600

    feat: finish task

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,22 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> combinedDeck = new ArrayDeque<>();
+        combinedDeck.add(firstQueue.poll());
+        combinedDeck.add(firstQueue.poll());
+        combinedDeck.add(secondQueue.poll());
+        combinedDeck.add(secondQueue.poll());
+
+        while (!(firstQueue.isEmpty() && secondQueue.isEmpty())) {
+            firstQueue.add(combinedDeck.pollLast());
+            combinedDeck.addLast(firstQueue.poll());
+            combinedDeck.addLast(firstQueue.poll());
+
+            secondQueue.add(combinedDeck.pollLast());
+            combinedDeck.addLast(secondQueue.poll());
+            combinedDeck.addLast(secondQueue.poll());
+        }
+
+        return combinedDeck;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,17 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        System.out.println("a".compareTo("b"));
+
+        PriorityQueue<String> result = new PriorityQueue<>((o1, o2) -> Integer.compare(0, o1.compareTo(o2)));
+        result.addAll(firstList);
+        result.addAll(secondList);
+
+        return result;
     }
 }
